dd9772362e4aae8655085b2260cfd91ef1a8a18f
Victor Souza <vicentesouzavictor29@gmail.com>
2026-01-27 18:20:58 -0300

    fiz um math random para sortear os numeros com nos dados

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let qtde_numeros_sorteados = 0;
+
 function sortear() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
@@ -7,5 +9,10 @@ function sortear() {
     alert(`de: ${de}`);
     alert(`ate: ${ate}`);
 
-    
+    while (qtde_numeros_sorteados < quantidade) {
+      let num_sorteados = parseInt(Math.random() * ate) + de;
+      alert(`Número sorteado: ${num_sorteados}`);
+
+      qtde_numeros_sorteados++;
+    }
 }

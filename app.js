91107302c5fb823172a831f083d16a7cab14e806
Victor Souza <vicentesouzavictor29@gmail.com>
2026-01-27 17:53:43 -0300

    Mensagem de commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`de: ${de}`);
+    alert(`ate: ${ate}`);
+
+    
+}

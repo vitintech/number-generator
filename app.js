c38c51ccd282c243430048e479e7095b984f4396
Victor Souza <vicentesouzavictor29@gmail.com>
2026-01-28 15:44:05 -0300

    adicionei comentários as funções

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let lista_nuSorteados = [];
 let i;
 
+// função para informações, testes, e exibir os numeros sorteados
 function sortear() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
@@ -22,10 +23,15 @@ function sortear() {
   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${lista_nuSorteados}</label>`;
 }
 
+// Função para chamar e gerar o numero
 function gerarNumeroAleatorio(min, max) {
     return parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
+// Função para mudar o status do botão reiniciar
+
+
+// Função que da ação ao botão reiniciar
 function reiniciar() {
     lista_nuSorteados = [];
     document.getElementById("resultado").innerHTML = "<label class=\"texto__paragrafo\">Números sorteados:  </label>";
